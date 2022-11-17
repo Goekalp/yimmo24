b5f5b344e6bf55dd511a620b4ad56382259c8b38
Goekalp <Goekalpyildiz@outlook.de>
2022-11-17 15:48:21 +0100

    Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,35 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <nav class="navbar">
+      <router-link class="navbar__brand" to="/">Home</router-link>
+      <ul class="navbar__links">
+        <li class="navbar__links__item">
+          <router-link to="/" class="navbar__links__item__a">Home</router-link>
+        </li>
+        <li class="navbar__links__item">
+          <router-link to="/" class="navbar__links__item__a">Home</router-link>
+        </li>
+        <li class="navbar__links__item">
+          <router-link to="/" class="navbar__links__item__a">Home</router-link>
+        </li>
+        <li class="navbar__links__item">
+          <router-link to="/" class="navbar__links__item__a">Home</router-link>
+        </li>
+        <li class="navbar__links__item">
+          <router-link to="/" class="navbar__links__item__a">Home</router-link>
+        </li>
+      </ul>
     </nav>
+
     <router-view />
   </div>
 </template>
 
 <style lang="scss">
+body {
+  margin: 0;
+  padding: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -17,15 +38,40 @@
   color: #2c3e50;
 }
 
-nav {
-  padding: 30px;
+.navbar {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  height: 5rem;
+  background: orange;
+  padding: 0 10%;
+  box-sizing: border-box;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+  &__links {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
 
-    &.router-link-exact-active {
-      color: #42b983;
+    &__item {
+      display: inline-block;
+      padding: 2rem;
+
+      &__a {
+        color: #ffffff;
+        font-size: 1rem;
+        font-weight: bold;
+        text-decoration: underline 0.1em rgba($color: #000000, $alpha: 0);
+        text-underline-offset: 0.1em;
+        transition: 2s all ease;
+      }
+      &__a:visited {
+        color: #ffffff;
+      }
+      &__a:hover {
+        color: #ff0000;
+        text-decoration-color: rgba($color: #ff0000, $alpha: 1);
+      }
     }
   }
 }
