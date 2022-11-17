5bc48c00b19b01d657c8ec05d94dfee2f6833127
Goekalp <Goekalpyildiz@outlook.de>
2022-11-17 18:53:45 +0100

    Bootstrap integrated

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -24,11 +24,69 @@
         </div>
       </div>
     </div>
-    <div class="container"></div>
+    <div class="container--row carousel">
+      <b-card-group>
+        <b-card
+          title="Title"
+          img-src="https://picsum.photos/300/300/?image=41"
+          img-alt="Image"
+          img-top
+        >
+          <b-card-text>
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This content is a little bit longer.
+          </b-card-text>
+          <template #footer>
+            <small class="text-muted">Last updated 3 mins ago</small>
+          </template>
+        </b-card>
+
+        <b-card
+          title="Title"
+          img-src="https://picsum.photos/300/300/?image=41"
+          img-alt="Image"
+          img-top
+        >
+          <b-card-text>
+            This card has supporting text below as a natural lead-in to
+            additional content.
+          </b-card-text>
+          <template #footer>
+            <small class="text-muted">Last updated 3 mins ago</small>
+          </template>
+        </b-card>
+
+        <b-card
+          title="Title"
+          img-src="https://picsum.photos/300/300/?image=41"
+          img-alt="Image"
+          img-top
+        >
+          <b-card-text>
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This card has even longer content than the
+            first to show that equal height action.
+          </b-card-text>
+          <template #footer>
+            <small class="text-muted">Last updated 3 mins ago</small>
+          </template>
+        </b-card>
+      </b-card-group>
+    </div>
     <div class="container"></div>
   </div>
 </template>
+<script>
+import { BCard, BCardGroup, BCardText } from "bootstrap-vue";
 
+export default {
+  components: {
+    "b-card": BCard,
+    "b-card-group": BCardGroup,
+    "b-card-text": BCardText,
+  },
+};
+</script>
 <style lang="scss" scoped>
 .container--col {
   display: flex;
@@ -112,5 +170,8 @@
     max-width: 100%;
     margin-bottom: -200px;
   }
+}
+.carousel {
+  height: 40vh;
 }
 </style>
