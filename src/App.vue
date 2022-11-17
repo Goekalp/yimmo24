f0f71c1a86f5a62be9d0605d3e6e8f3b40cc274f
Goekalp <Goekalpyildiz@outlook.de>
2022-11-17 16:26:50 +0100

    Navbar muster

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,9 @@
           <router-link to="/" class="navbar__links__item__a">Home</router-link>
         </li>
         <li class="navbar__links__item">
-          <router-link to="/" class="navbar__links__item__a">Home</router-link>
+          <router-link to="/" class="navbar__links__item--highlight"
+            >Contact</router-link
+          >
         </li>
       </ul>
     </nav>
@@ -26,6 +28,12 @@
 </template>
 
 <style lang="scss">
+:root {
+  --color-primary: #ff731d;
+  --color-text: #2c3e50;
+  --color-white-1: #ffffff;
+}
+
 body {
   margin: 0;
   padding: 0;
@@ -43,36 +51,72 @@ body {
   align-items: center;
   justify-content: space-between;
   width: 100%;
-  height: 5rem;
-  background: orange;
-  padding: 0 10%;
+  height: 4.5rem;
+  background: #fff7e9;
+  padding: 0 2rem;
   box-sizing: border-box;
 
   &__links {
     list-style-type: none;
     margin: 0;
     padding: 0;
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+    width: 40%;
+    height: 100%;
 
     &__item {
       display: inline-block;
-      padding: 2rem;
+      width: 10rem;
+      margin: 3rem;
 
       &__a {
-        color: #ffffff;
+        color: var(--color-text);
         font-size: 1rem;
         font-weight: bold;
         text-decoration: underline 0.1em rgba($color: #000000, $alpha: 0);
         text-underline-offset: 0.1em;
-        transition: 2s all ease;
+        transition: 0.5s all ease;
       }
       &__a:visited {
-        color: #ffffff;
+        color: var(--color-text);
       }
       &__a:hover {
-        color: #ff0000;
-        text-decoration-color: rgba($color: #ff0000, $alpha: 1);
+        color: var(--color-primary);
+        text-decoration-color: rgba($color: var(--color-primary), $alpha: 1);
+      }
+
+      &--highlight {
+        background: #5f9df7;
+        margin-left: 10px;
+        padding: 15px 40px;
+
+        border-radius: 10px;
+        transition: 0.5s background-color ease;
+        cursor: pointer;
+        color: var(--color-white-1);
+        font-size: 1rem;
+        font-weight: bold;
+        text-decoration: none;
+      }
+      &--highlight:hover {
+        background-color: #1746a2;
       }
     }
+  }
+
+  &__brand {
+    color: var(--color-text);
+    font-size: 3em;
+    text-decoration: none;
+    font-family: Georgia, "Times New Roman", Times, serif;
+    font-weight: bold;
+    margin: 0 3rem;
+  }
+  &__brand:visited {
+    color: var(--color-text);
+    text-decoration: none;
   }
 }
 </style>
